9cf526f650ba1daa66d906bd558336b4a72dafed
lux-ops <ops@warpprotocol.xyz>
2026-04-04 09:30:00 +0000

    feat(sdk): wire public exports

---
+++ packages/sdk/src/index.ts
@@ -0,0 +1,12 @@
+export { WarpClient, WarpApiError } from "./client";
+export type {
+  AgentVerdict,
+  VerdictResponse,
+  VerdictReasonInfo,
+  BatchQueryRequest,
+  BatchQueryOptions,
+  BatchQueryResponse,
+  WarpClientConfig,
+  FeedMessage,
+  VerdictCallback,
+} from "./types";
